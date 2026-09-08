ec47867d03adb1456bcec47374d51f711f46c326
gdrex0116 <johans.arcilal@uqvirtual.edu.co>
2026-09-08 10:10:27 -0500

    commit2

--- untitled/src/Main.java
+++ untitled/src/Main.java
@@ -1,5 +1,6 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
+
+public String palabra = "Hola mundo;"// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 void main() {
     //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
     // to see how IntelliJ IDEA suggests fixing it.
